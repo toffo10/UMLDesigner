be1ef3d12085371a7551e8d580270308120c23bd
Enrico <n>
2022-03-09 12:49:32 +0100

    Aggiunta textArea per esito parsing

--- src/main/code/compiler/Parser.java
+++ src/main/code/compiler/Parser.java
@@ -4,19 +4,23 @@ import compiler.generated.UmlDesignerParser;
 import org.antlr.runtime.CommonTokenStream;
 
 public class Parser {
-	static UmlDesignerParser parser;
+    static UmlDesignerParser parser;
 
-	public static void doParsing(String input) {
-		try {
-			System.out.println ("Parsing con ANTLR lexer");
-			System.out.println ("-----------------------");
-			parser = new UmlDesignerParser(input);
-			parser.initUml();
-			System.out.println ("Parsing terminato correttamente");
-		} catch (Exception e) {
-			System.out.println ("Parsing con ANTLR abortito\n\n");
-			e.printStackTrace();
-		}
-		System.out.println ("");
-	}
+    public static StringBuffer sb = new StringBuffer();
+
+    public static void doParsing(String input) {
+        sb.delete(0, sb.length());
+
+        try {
+            sb.append("Parsing con ANTLR lexer\n");
+            sb.append("-----------------------\n");
+            parser = new UmlDesignerParser(input);
+            parser.initUml();
+            sb.append("Parsing terminato correttamente\n");
+        } catch (Exception e) {
+            sb.append("Parsing con ANTLR abortito\n");
+            sb.append(e.getMessage());
+            sb.append("\n");
+        }
+    }
 }
